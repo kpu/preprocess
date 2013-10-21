9a335e7840ef609f8fe1df70131d46900053691b
Kenneth Heafield <github@kheafield.com>
2013-10-21 11:50:56 -0700

    Switch to probing hash table for dedupe

--- preprocess/dedupe_main.cc
+++ preprocess/dedupe_main.cc
@@ -1,20 +1,45 @@
 #include "util/fake_ofstream.hh"
 #include "util/file_piece.hh"
 #include "util/murmur_hash.hh"
+#include "util/probing_hash_table.hh"
+#include "util/scoped.hh"
 
-#include <boost/unordered_set.hpp>
+#include <boost/lexical_cast.hpp>
 
 #include <iostream>
 
-int main() {
+#include <stdint.h>
+
+struct Entry {
+  typedef uint64_t Key;
+  uint64_t key;
+  uint64_t GetKey() const { return key; }
+  void SetKey(uint64_t to) { key = to; }
+};
+
+int main(int argc, char *argv[]) {
+  std::size_t estimate = (argc == 2) ? boost::lexical_cast<std::size_t>(argv[1]) : 100000000;
+  typedef util::ProbingHashTable<Entry, util::IdentityHash> Table;
   try {
-    boost::unordered_set<uint64_t> hashes;
+    util::scoped_malloc table_backing(util::CallocOrThrow(Table::Size(estimate, 1.5)));
+    Table table(table_backing.get(), Table::Size(estimate, 1.5));
+    std::size_t double_cutoff = estimate * 1.2;
     util::FakeOFStream out(1);
     util::FilePiece in(0, "stdin", &std::cerr);
     while (true) {
       StringPiece l = in.ReadLine();
-      if (hashes.insert(util::MurmurHashNative(l.data(), l.size())).second) {
+      Entry entry;
+      Table::MutableIterator it;
+      entry.key = util::MurmurHashNative(l.data(), l.size());
+      if (!table.FindOrInsert(entry, it)) {
         out << l << '\n';
+        if (table.SizeNoSerialization() > double_cutoff) {
+          table_backing.call_realloc(table.DoubleTo());
+          table.Double(table_backing.get());
+          double_cutoff *= 2;
+        }
+      } else {
+        std::cerr << "Already seen " << entry.key << ' ' << l << std::endl;
       }
     }
   } 
