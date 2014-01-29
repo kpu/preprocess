e59f6165bcfe3430c427ccd117af359540e62ae9
Kenneth Heafield <github@kheafield.com>
2014-01-29 12:53:03 -0800

    State how much was kept, update probing hash table from kenlm

--- preprocess/dedupe_main.cc
+++ preprocess/dedupe_main.cc
@@ -18,32 +18,30 @@ struct Entry {
 };
 
 int main(int argc, char *argv[]) {
-  std::size_t estimate = (argc == 2) ? boost::lexical_cast<std::size_t>(argv[1]) : 100000000;
-  typedef util::ProbingHashTable<Entry, util::IdentityHash> Table;
   try {
-    util::scoped_malloc table_backing(util::CallocOrThrow(Table::Size(estimate, 1.5)));
-    Table table(table_backing.get(), Table::Size(estimate, 1.5));
-    std::size_t double_cutoff = estimate * 1.2;
-    util::FakeOFStream out(1);
+    std::size_t estimate = (argc == 2) ? boost::lexical_cast<std::size_t>(argv[1]) : 100000000;
+    typedef util::AutoProbing<Entry, util::IdentityHash> Table;
+    Table table(estimate);
+    uint64_t lines = 0;
     util::FilePiece in(0, "stdin", NULL);
+    util::FakeOFStream out(1);
+    StringPiece l;
     while (true) {
-      StringPiece l = in.ReadLine();
+      try {
+        l = in.ReadLine();
+      } catch (const util::EndOfFileException &e) { break; }
+      ++lines;
       Entry entry;
       Table::MutableIterator it;
       entry.key = util::MurmurHashNative(l.data(), l.size()) + 1;
       if (!table.FindOrInsert(entry, it)) {
         out << l << '\n';
-        if (table.SizeNoSerialization() > double_cutoff) {
-          table_backing.call_realloc(table.DoubleTo());
-          table.Double(table_backing.get());
-          double_cutoff *= 2;
-        }
       }
     }
-  } 
-  catch (const util::EndOfFileException &e) { return 0; }
-  catch (const std::exception &e) {
+    std::cerr << "Kept " << table.Size() << " / " << lines << " = " << (static_cast<float>(table.Size()) / static_cast<float>(lines)) << std::endl;
+  } catch (const std::exception &e) {
     std::cerr << e.what() << std::endl;
     return 1;
   }
+  return 0;
 }
