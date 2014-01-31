66e907923fc247e449c381b4e4df2f69822235a2
Kenneth Heafield <github@kheafield.com>
2014-01-30 18:15:09 -0800

    Factor out parallel corpus handling, update from kenlm

--- preprocess/dedupe_main.cc
+++ preprocess/dedupe_main.cc
@@ -1,5 +1,4 @@
-#include "util/fake_ofstream.hh"
-#include "util/file_piece.hh"
+#include "preprocess/parallel.hh"
 #include "util/murmur_hash.hh"
 #include "util/probing_hash_table.hh"
 #include "util/scoped.hh"
@@ -17,31 +16,21 @@ struct Entry {
   void SetKey(uint64_t to) { key = to; }
 };
 
-int main(int argc, char *argv[]) {
-  try {
-    std::size_t estimate = (argc == 2) ? boost::lexical_cast<std::size_t>(argv[1]) : 100000000;
-    typedef util::AutoProbing<Entry, util::IdentityHash> Table;
-    Table table(estimate);
-    uint64_t lines = 0;
-    util::FilePiece in(0, "stdin", NULL);
-    util::FakeOFStream out(1);
-    StringPiece l;
-    while (true) {
-      try {
-        l = in.ReadLine();
-      } catch (const util::EndOfFileException &e) { break; }
-      ++lines;
+class Dedupe {
+  public:
+    bool operator()(const StringPiece &line) {
       Entry entry;
+      entry.key = util::MurmurHashNative(line.data(), line.size()) + 1;
       Table::MutableIterator it;
-      entry.key = util::MurmurHashNative(l.data(), l.size()) + 1;
-      if (!table.FindOrInsert(entry, it)) {
-        out << l << '\n';
-      }
+      return !table_.FindOrInsert(entry, it);
     }
-    std::cerr << "Kept " << table.Size() << " / " << lines << " = " << (static_cast<float>(table.Size()) / static_cast<float>(lines)) << std::endl;
-  } catch (const std::exception &e) {
-    std::cerr << e.what() << std::endl;
-    return 1;
-  }
-  return 0;
+
+  private:
+    typedef util::AutoProbing<Entry, util::IdentityHash> Table;
+    Table table_;
+};
+
+int main(int argc, char *argv[]) {
+  Dedupe dedupe;
+  return FilterParallel(dedupe, argc, argv);
 }
--- preprocess/select_latin.cc
+++ preprocess/select_latin.cc
@@ -1,3 +1,4 @@
+#include "preprocess/parallel.hh"
 #include "util/fake_ofstream.hh"
 #include "util/file_piece.hh"
 
@@ -34,53 +35,7 @@ struct SelectLatin {
   }
 };
 
-template <class Pass> int FilterParallel(const Pass &pass, int argc, char **argv) {
-  uint64_t input = 0, output = 0;
-  if (argc == 1) {
-    StringPiece line;
-    util::FilePiece in(0);
-    util::FakeOFStream out(1);
-    while (true) {
-      try {
-        line = in.ReadLine();
-      } catch (const util::EndOfFileException &e) { break; }
-      ++input;
-      if (pass(line)) {
-        out << line << '\n';
-        ++output;
-      }
-    }
-  } else if (argc == 5) {
-    StringPiece line0, line1;
-    util::FilePiece in0(argv[1], &std::cerr), in1(argv[2]);
-    util::FakeOFStream out0(util::CreateOrThrow(argv[3])), out1(util::CreateOrThrow(argv[4]));
-    while (true) {
-      try {
-        line0 = in0.ReadLine();
-      } catch (const util::EndOfFileException &e) { break; }
-      line1 = in1.ReadLine();
-      ++input;
-      if (pass(line0) && pass(line1)) {
-        out0 << line0 << '\n';
-        out1 << line1 << '\n';
-        ++output;
-      }
-    }
-    try {
-      line1 = in1.ReadLine();
-      std::cerr << "Input is not balaced: " << argv[2] << " has " << line1 << std::endl;
-      return 2;
-    } catch (const util::EndOfFileException &e) {}
-  } else {
-    std::cerr << 
-      "To filter one file, run\n" << argv[0] << " <stdin >stdout\n"
-      "To filter parallel files, run\n" << argv[0] << "in0 in1 out0 out1\n";
-    return 1;
-  }
-  std::cerr << "Kept " << input << " / " << output << " = " << (static_cast<float>(output) / static_cast<float>(input)) << std::endl;
-  return 0;
-}
-
 int main(int argc, char *argv[]) {
-  return FilterParallel(SelectLatin(), argc, argv);
+  SelectLatin process;
+  return FilterParallel(process, argc, argv);
 }
