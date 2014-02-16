003cfa7fb45b3d0c9567009a116c326b69a1b6f7
Kenneth Heafield <github@kheafield.com>
2014-02-16 11:52:13 -0800

    Vocab uses AutoProbing now

--- preprocess/vocab_main.cc
+++ preprocess/vocab_main.cc
@@ -1,12 +1,21 @@
 #include "util/file_piece.hh"
 #include "util/fake_ofstream.hh"
 #include "util/murmur_hash.hh"
+#include "util/probing_hash_table.hh"
 
 #include <boost/unordered_set.hpp>
 
 #include <iostream>
 
 #include <string.h>
+
+struct Entry {
+  typedef uint64_t Key;
+  uint64_t key;
+  uint64_t GetKey() const { return key; }
+  void SetKey(uint64_t to) { key = to; }
+};
+
 
 int main() {
   bool delimiters[256];
@@ -17,14 +26,18 @@ int main() {
   delimiters['\n'] = true;
   delimiters[' '] = true;
 
-  boost::unordered_set<uint64_t> seen;
+  util::AutoProbing<Entry, util::IdentityHash> seen;
 
   util::FilePiece in(0, "stdin", &std::cerr);
   util::FakeOFStream out(1);
 
+  util::AutoProbing<Entry, util::IdentityHash>::MutableIterator it;
+  Entry entry;
+
   try { while (true) {
     StringPiece word = in.ReadDelimited(delimiters);
-    if (seen.insert(util::MurmurHashNative(word.data(), word.size())).second) {
+    entry.SetKey(util::MurmurHashNative(word.data(), word.size()));
+    if (!seen.FindOrInsert(entry, it)) {
       out << word << '\0';
     }
   } } catch (const util::EndOfFileException &e) {}
