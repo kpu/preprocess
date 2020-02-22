4b543b6c858778fc10daa481405c7ec8d2407f3d
Kenneth Heafield <github@kheafield.com>
2020-02-22 19:16:08 +0000

    Restore PCQueue with C++11 style

--- util/pcqueue.hh
+++ util/pcqueue.hh
@@ -14,8 +14,8 @@
 #include <boost/interprocess/sync/interprocess_semaphore.hpp>
 #endif
 
+#include <mutex>
 #include <cerrno>
-
 
 namespace util {
 
@@ -104,6 +104,84 @@ inline void WaitSemaphore (Semaphore &on) {
 }
 
 #endif // Apple
+
+/**
+ * Producer consumer queue safe for multiple producers and multiple consumers.
+ * T must be default constructable and have operator=.
+ * The value is copied twice for Consume(T &out) or three times for Consume(),
+ * so larger objects should be passed via pointer.
+ * Strong exception guarantee if operator= throws.  Undefined if semaphores throw.
+ */
+template <class T> class PCQueue {
+ public:
+  explicit PCQueue(size_t size)
+   : empty_(size), used_(0),
+     storage_(new T[size]),
+     end_(storage_.get() + size),
+     produce_at_(storage_.get()),
+     consume_at_(storage_.get()) {}
+
+  // Add a value to the queue.
+  void Produce(const T &val) {
+    WaitSemaphore(empty_);
+    {
+      std::lock_guard<std::mutex> produce_lock(produce_at_mutex_);
+      try {
+        *produce_at_ = val;
+      }
+      catch (...) {
+        empty_.post();
+        throw;
+      }
+      if (++produce_at_ == end_) produce_at_ = storage_.get();
+    }
+    used_.post();
+  }
+
+  // Consume a value, assigning it to out.
+  T& Consume(T &out) {
+    WaitSemaphore(used_);
+    {
+      std::lock_guard<std::mutex> consume_lock(consume_at_mutex_);
+      try {
+        out = *consume_at_;
+      }
+      catch (...) {
+        used_.post();
+        throw;
+      }
+      if (++consume_at_ == end_) consume_at_ = storage_.get();
+    }
+    empty_.post();
+    return out;
+  }
+
+  // Convenience version of Consume that copies the value to return.
+  // The other version is faster.
+  T Consume() {
+    T ret;
+    Consume(ret);
+    return ret;
+  }
+
+ private:
+  // Number of empty spaces in storage_.
+  Semaphore empty_;
+  // Number of occupied spaces in storage_.
+  Semaphore used_;
+
+  std::unique_ptr<T[]> storage_;
+
+  T *const end_;
+
+  // Index for next write in storage_.
+  T *produce_at_;
+  std::mutex produce_at_mutex_;
+
+  // Index for next read from storage_.
+  T *consume_at_;
+  std::mutex consume_at_mutex_;
+};
 
 template <class T> struct UnboundedPage {
   UnboundedPage() : next(nullptr) {}
