81c9bbeef5ba4ac183ce82e77f2d9eeaf10eb058
Kenneth Heafield <github@kheafield.com>
2021-04-25 13:35:54 +0100

    Regression test for shard compressed output

--- preprocess/tests/shard/run.sh
+++ preprocess/tests/shard/run.sh
@@ -4,4 +4,8 @@
 diff <(sort "$TMP"/test_a "$TMP"/test_b) <(sort "$CUR"/input)
 "$BIN/shard" --prefix "$TMP"/test --number 4 <"$CUR"/input
 diff <(sort "$TMP"/test{0,1,2,3}) <(sort "$CUR"/input)
+"$BIN/shard" --prefix "$TMP"/test -c gzip --number 4 <"$CUR"/input
+diff <(zcat "$TMP"/test{0,1,2,3} |sort) <(sort "$CUR"/input)
+"$BIN/shard" --prefix "$TMP"/test -c bzip2 --number 4 <"$CUR"/input
+diff <(bzcat "$TMP"/test{0,1,2,3} |sort) <(sort "$CUR"/input)
 rm "$TMP"/test_a "$TMP"/test_b "$TMP"/test{0,1,2,3}
